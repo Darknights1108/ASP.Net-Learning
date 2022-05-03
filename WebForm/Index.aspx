12bebf74bdbc008680c1206f4aeccd76e5590f44
Darknights1108 <ansonangzhenloong@gmail.com>
2022-05-04 01:07:29 +0800

    新建项目

---
+++ WebForm/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebForm.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TB_UserName" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="TB_Password" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button_Login" runat="server" Text="Login" OnClick="Button_Login_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Login_Click(object sender, EventArgs e)
+        {
+            if (TB_UserName.Text == "admin")
+            {
+                if (TB_Password.Text == "1234")
+                {
+                    Response.Redirect("/Home.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('登录失败 密码错误！')</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('用户不存在！')</script>");
+            }
+        }
+    }
+}
